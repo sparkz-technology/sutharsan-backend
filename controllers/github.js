29e1554d32757038b1fc18cf22841293f6e8b1f1
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-10-28 17:38:20 +0530

    bug_

--- controllers/github.js
+++ controllers/github.js
@@ -7,20 +7,23 @@ export const githubLogin =  passport.authenticate('github', { scope: [ 'user:ema
 
 export const githubCallback = (req, res) => {
   passport.authenticate('github', {
-    failureRedirect: '/',
-    successRedirect: `${ClIEND_URL}admin`,
     session: false,
   }, (authErr, user, info) => {
-    if (authErr) {
-      console.log(authErr);
+    if (authErr || !user) {
+      console.log(authErr || "User not found");
       // handle authentication error
       return res.redirect(`${ClIEND_URL}`);
     }
     
-    const token = jwt.sign({ id: user.githubId ,userId:user._id}, JWT_SECRET, { expiresIn: '1d' });
-    res.cookie('token', token, { maxAge: 86400000 });
+    const token = jwt.sign({ id: user.githubId, userId: user._id }, JWT_SECRET);
+    res.cookie('token', token, { 
+      maxAge: 86400000,
+      httpOnly: true, // Secure the cookie
+      // other cookie settings if needed
+    });
     return res.redirect(`${ClIEND_URL}admin`);
   })(req, res);
 };
+
 
 
