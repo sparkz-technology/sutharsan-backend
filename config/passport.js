1e4f19fa1bb581e72f090c9962bb39ecb623c05e
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-10-29 10:52:46 +0530

    bug_gone

--- config/passport.js
+++ config/passport.js
@@ -4,14 +4,23 @@ import constant from './constant.js';
 import User from '../models/user.js';
 
 passport.use(
-  new GitHubStrategy.Strategy(
-    constant.github,
-    async (accessToken, refreshToken, profile, done) => {
+  new GitHubStrategy(
+    {
+      clientID: 'be5a9847d65b1697867d',
+      clientSecret: 'c4659c3c1d48741b6713be5cd775412a685a8007',
+      callbackURL: 'http://localhost:8000/auth/github/callback',
+      passReqToCallback: true
+    },
+    async (req, accessToken, refreshToken, profile, done) => {
       try {
+        if (!profile) {
+          return done(null, false, { message: 'You are not authorized to access this page' });
+        }
+
         let user = await User.findOne({ githubId: profile.id });
 
         if (!user) {
-          return done(null, false, { message: 'You are not authorized to access this page' });
+        return done(null, false, { message: 'You are not authorized to access this page' });
         }
 
         done(null, user);
@@ -23,8 +32,9 @@ passport.use(
   )
 );
 
+
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  done(null, user);
 });
 
 passport.deserializeUser(async (id, done) => {
@@ -35,4 +45,3 @@ passport.deserializeUser(async (id, done) => {
     done(err);
   }
 });
-
--- controllers/github.js
+++ controllers/github.js
@@ -1,29 +1,25 @@
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
 import constant from '../config/constant.js';
-const { JWT_SECRET,ClIEND_URL } = constant;
-
+const { JWT_SECRET,CLIEND_URL } = constant;
 export const githubLogin =  passport.authenticate('github', { scope: [ 'user:email' ] });
 
-export const githubCallback = (req, res) => {
+export const githubCallback = (req, res, next) => {
   passport.authenticate('github', {
-    session: false,
-  }, (authErr, user, info) => {
-    if (authErr || !user) {
-      console.log(authErr || "User not found");
-      // handle authentication error
-      return res.redirect(`${ClIEND_URL}`);
+    failureRedirect: `${CLIEND_URL}login`,
+    successRedirect: `${CLIEND_URL}admin`,
+    session: false }, async (err, user) => {
+    if (err) {
+      return next(err);
+        
     }
-    
-    const token = jwt.sign({ id: user.githubId, userId: user._id }, JWT_SECRET);
-    res.cookie('token', token, { 
-      maxAge: 86400000,
-      httpOnly: true, // Secure the cookie
-      // other cookie settings if needed
-    });
-    return res.redirect(`${ClIEND_URL}admin`);
-  })(req, res);
+    if (!user) {
+      return res.redirect(`${CLIEND_URL}login`);
+    }
+    const token =  jwt.sign({ id: user.githubId, userId: user._id }, JWT_SECRET, { expiresIn: '1d' });
+    res.cookie('token', token, { maxAge: 86400000 });
+    return res.redirect(`${CLIEND_URL}admin`);
+  })(req, res, next);
 };
-
 
 
