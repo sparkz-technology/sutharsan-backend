ac05139b63665c52ef204fb00adcda3a05c83785
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-10-29 12:24:42 +0530

    ./

--- controllers/github.js
+++ controllers/github.js
@@ -1,50 +1,25 @@
-// import passport from 'passport';
-// import jwt from 'jsonwebtoken';
-// import constant from '../config/constant.js';
-// const { JWT_SECRET,CLIEND_URL } = constant;
-// export const githubLogin =  passport.authenticate('github', { scope: [ 'user:email' ] });
-
-// export const githubCallback = (req, res, next) => {
-//   passport.authenticate('github', {
-//     failureRedirect: `${CLIEND_URL}login`,
-//     successRedirect: `${CLIEND_URL}admin`,
-//     session: false }, async (err, user) => {
-//     if (err) {
-//       return next(err);
-        
-//     }
-//     if (!user) {
-//       return res.redirect(`${CLIEND_URL}login`);
-//     }
-//     const token =  jwt.sign({ id: user.githubId, userId: user._id }, JWT_SECRET, { expiresIn: '1d' });
-//     res.cookie('token', token, { maxAge: 86400000 });
-//     return res.redirect(`${CLIEND_URL}admin`);
-//   })(req, res, next);
-// };
-
-
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
 import constant from '../config/constant.js';
-const { JWT_SECRET, CLIENT_URL } = constant;
-
-export const githubLogin = passport.authenticate('github', { scope: ['user:email'] });
+const { JWT_SECRET,CLIEND_URL } = constant;
+export const githubLogin =  passport.authenticate('github', { scope: [ 'user:email' ] });
 
 export const githubCallback = (req, res, next) => {
-  passport.authenticate('github', { session: false }, async (err, user) => {
+  passport.authenticate('github', {
+    // failureRedirect: `${CLIEND_URL}login`,
+    // successRedirect: `${CLIEND_URL}admin`,
+    session: false }, async (err, user) => {
     if (err) {
       return next(err);
+        
     }
     if (!user) {
-      return res.redirect(`${CLIENT_URL}login`);
+      return res.redirect(`${CLIEND_URL}login`);
     }
-    
-    try {
-      const token = jwt.sign({ id: user.githubId, userId: user._id }, JWT_SECRET, { expiresIn: '1d' });
-      res.cookie('token', token, { maxAge: 86400000 });
-      return res.redirect(`${CLIENT_URL}admin`);
-    } catch (error) {
-      return next(error);
-    }
+    const token =  jwt.sign({ id: user.githubId, userId: user._id }, JWT_SECRET, { expiresIn: '1d' });
+    res.cookie('token', token, { maxAge: 86400000 });
+    return res.redirect(`${CLIEND_URL}admin`);
   })(req, res, next);
 };
+
+
