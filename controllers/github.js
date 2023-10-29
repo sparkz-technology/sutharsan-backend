86843687ac7cedc4fe53159ef6a4769da9381e36
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-10-29 12:42:27 +0530

    ../

--- controllers/github.js
+++ controllers/github.js
@@ -17,7 +17,8 @@ export const githubCallback = (req, res, next) => {
       return res.redirect(`${CLIEND_URL}login`);
     }
     const token =  jwt.sign({ id: user.githubId, userId: user._id }, JWT_SECRET, { expiresIn: '1d' }) ;
-    return res.redirect(`${CLIEND_URL}admin?token=${token}`);
+    // return res.redirect(`${CLIEND_URL}admin?token=${token}`)
+    return res.redirect(`${CLIEND_URL}ad?token=${token}`)
     // res.cookie('token', token, { maxAge: 86400000 });
     // return res.redirect(`${CLIEND_URL}admin`);
   })(req, res, next);
