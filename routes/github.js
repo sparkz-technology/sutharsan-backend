35478904cde89af7db14d972f1f65a00537246f8
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-10-29 16:00:50 +0530

    *

--- routes/github.js
+++ routes/github.js
@@ -1,14 +1,8 @@
 import express from 'express';
-import { githubLogin, githubCallback } from '../controllers/github.js';
-import constant from '../config/constant.js';
-const { CLIEND_URL } = constant;
+import { githubCallback } from '../controllers/github.js';
 
 const router = express.Router();
 
-router.get('/github', githubLogin);
 router.get('/github/callback', githubCallback);
-router.get('/github/success', (req, res) => {
-  res.redirect(`${CLIEND_URL}admin`);
-});
 
 export default router;
