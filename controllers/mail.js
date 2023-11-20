1a5628583c4e7334208141320a86298a44eb0279
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-11-20 21:13:25 +0530

    Fix import statement and add whitespace for readability

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,5 +1,5 @@
 import sendMail from "../services/mail.js";
-import {constant}from "../config/constant.js";
+import constant from "../config/constant.js";
 import { logEmail } from "../utils/logger.js";
 
 export const sendMessages = async (req, res, next) => {
@@ -9,7 +9,7 @@ export const sendMessages = async (req, res, next) => {
     const subject = `Message from ${name}`;
     const text = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
     const info = await sendMail({ subject, text });
-    if(constant.NODE_ENV === "development"){
+    if (constant.NODE_ENV === "development") {
       logEmail(`Email sent: ${info.response}`);
     }
     res.status(200).json(info);
